a794c9139f53a7b79c337e793886ba61f419186f
Abhijeet Kumar <abhijeet.kumar@abhijeetvrs-33.successivetech.com>
2025-07-07 16:42:02 +0530

    fixed css files

--- src/app/assignment-6/question-1/About/About.jsx
+++ src/app/assignment-6/question-1/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   const router = useRouter();
   return (
     <div>
-      <p>This is and About Page, </p>
+      <p>This is About Page </p>
       <button onClick={() => router.push("/assignment-6/question-1")}>
         Go back
       </button>
